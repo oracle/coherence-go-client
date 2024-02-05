709ebdef84094ed8e876cd37c4a8307186eed5c0
Tim Middleton <tim.middleton@oracle.com>
2024-02-05 10:28:07 +0800

    Update examples test to add pause

--- scripts/run-compat-commercial.sh
+++ scripts/run-compat-commercial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2022, 2023 Oracle and/or its affiliates.
+# Copyright (c) 2022, 2024 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # https://oss.oracle.com/licenses/upl.
 #
@@ -10,8 +10,8 @@
 set -e
 
 # Coherence 14.1.1.2206.6 - javax
-echo "Coherence GE 14.1.1.2206.5"
-COHERENCE_VERSION=14.1.1-2206-5 COHERENCE_GROUP_ID=com.oracle.coherence make clean generate-proto build-test-images test-e2e-standalone
+echo "Coherence GE 14.1.1.2206.7"
+COHERENCE_VERSION=14.1.1-2206-7 COHERENCE_GROUP_ID=com.oracle.coherence make clean generate-proto build-test-images test-e2e-standalone
 
 echo "Coherence GE 15.1.1-0-0-SNAPSHOT" - Jakarta
 PROFILES=,jakarta,-javax COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 COHERENCE_VERSION=15.1.1-0-0-SNAPSHOT COHERENCE_GROUP_ID=com.oracle.coherence make clean generate-proto build-test-images test-e2e-standalone
