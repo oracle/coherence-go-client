0938f78e77f329073428e3c2d2376cc9d4164e63
Tim Middleton <tim.middleton@oracle.com>
2025-02-18 13:04:52 +0800

    Update docker images (#112)
    * Update docker images
    * Fix incorrect images

--- scripts/run-compat-commercial.sh
+++ scripts/run-compat-commercial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2022, 2024 Oracle and/or its affiliates.
+# Copyright (c) 2022, 2025 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # https://oss.oracle.com/licenses/upl.
 #
@@ -16,6 +16,6 @@ COHERENCE_VERSION=14.1.1-2206-7 COHERENCE_GROUP_ID=com.oracle.coherence make cle
 echo "Coherence GE 15.1.1-0-0-SNAPSHOT" - Jakarta
 PROFILES=,jakarta,-javax COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 COHERENCE_VERSION=15.1.1-0-0-SNAPSHOT COHERENCE_GROUP_ID=com.oracle.coherence make clean generate-proto build-test-images test-e2e-standalone
 
-COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 PROFILES=,-jakarta,javax COHERENCE_GROUP_ID=com.oracle.coherence COHERENCE_VERSION=14.1.2-0-1-SNAPSHOT make clean generate-proto generate-proto-v1 build-test-images test-e2e-standalone
+COHERENCE_BASE_IMAGE=gcr.io/distroless/java17-debian12 PROFILES=,-jakarta,javax COHERENCE_GROUP_ID=com.oracle.coherence COHERENCE_VERSION=14.1.2-0-1-SNAPSHOT make clean generate-proto generate-proto-v1 build-test-images test-e2e-standalone
 
-COHERENCE_BASE_IMAGE=gcr.io/distroless/java17 PROFILES=,-jakarta,javax,queues COHERENCE_GROUP_ID=com.oracle.coherence COHERENCE_VERSION=14.1.2-0-1-SNAPSHOT make clean generate-proto generate-proto-v1 build-test-images test-e2e-standalone-queues
+COHERENCE_BASE_IMAGE=gcr.io/distroless/java11-debian12 PROFILES=,-jakarta,javax,queues COHERENCE_GROUP_ID=com.oracle.coherence COHERENCE_VERSION=14.1.2-0-1-SNAPSHOT make clean generate-proto generate-proto-v1 build-test-images test-e2e-standalone-queues
